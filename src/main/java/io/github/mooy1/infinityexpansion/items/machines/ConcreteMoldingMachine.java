0143b3aec3ad4d03b78bdd8c0d69210d402cade2
baoad <3200961931@QQ.com>
2021-06-12 14:45:13 +0800

    简中

---
+++ src/main/java/io/github/mooy1/infinityexpansion/items/machines/ConcreteMoldingMachine.java
@@ -0,0 +1,31 @@
+package io.github.mooy1.infinityexpansion.items.machines;
+
+import io.github.thebusybiscuit.slimefun4.core.attributes.RecipeDisplayItem;
+import me.mrCookieSlime.Slimefun.Lists.RecipeType;
+import me.mrCookieSlime.Slimefun.Objects.Category;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.abstractItems.AContainer;
+import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.Nonnull;
+
+public class ConcreteMoldingMachine extends AContainer implements RecipeDisplayItem {
+
+
+    public ConcreteMoldingMachine(Category category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
+        super(category, item, recipeType, recipe);
+    }
+
+    @Override
+    public ItemStack getProgressBar() {
+        return new ItemStack(Material.WATER_BUCKET);
+    }
+
+    @Nonnull
+    @Override
+    public String getMachineIdentifier() {
+        return "CONCRETE_MOLDING_MACHINE";
+    }
+}
+
